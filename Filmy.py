39bc2447fb632e0e59a9b78112c7a54f72cdc1ec
Krzysztof Lach <lachkrzysztof56@gmail.com>
2021-06-22 00:52:20 +0200

    Create general template

--- Filmy.py
+++ Filmy.py
@@ -1,4 +1,4 @@
-
+import random
 
 class Film:
     def __init__(self, name, year, sort):
@@ -8,11 +8,44 @@ class Film:
 
         self.number_of_plays = 0
 
+    def __str__(self):
+        return f'{self.name} ({self.year})'
+
+    def __repr__(self):
+        return f'Film(name={self.name}, year={self.year}, sort={self.sort}'
+
     def play(self, step=1):
         self.number_of_plays += step
+
 
 class Serial(Film):
     def __init__(self, season, episode, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.season = season
         self.episode = episode
+
+    def __str__(self):
+        return f'{self.name} S{self.season}E{self.episode}'
+
+    def __repr__(self):
+        return f'Serial(name={self.name}, season={self.season}, episode={self.episode}, year={self.year}, sort={self.sort}'
+
+
+def get_movies():
+    list_movies = []
+    return sorted(list_movies)
+
+def get_series():
+    list_series = []
+    return sorted(list_series)
+
+def search(name):
+    pass
+
+def generate_views():
+    movie = random(films_end_series)
+
+def top_titles(content_type):
+    pass
+
+films_end_series = []
